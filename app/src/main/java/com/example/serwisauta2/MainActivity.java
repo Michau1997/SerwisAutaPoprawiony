acaba6a0376b85426ca6b93af0ae9fb93e21ee15
Michau1997 <michal.zawojski1@gmail.com>
2020-10-12 13:59:58 +0200

    Poprawiona aplikacja, dodawanie uzytkownikow do bazy danych, poprawiony odczyt nazwy uzytkownika

---
+++ app/src/main/java/com/example/serwisauta2/MainActivity.java
@@ -0,0 +1,163 @@
+package com.example.serwisauta2;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
+
+
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private RecyclerView postList;
+    private Toolbar mToolbar;
+    private TextView AddCar;
+    private TextView NavProfileUserName;
+
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference UsersRef;
+
+    String currentUserID;
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if(currentUser == null)
+        {
+            SendUserToLoginActivity();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        AddCar = (TextView) findViewById(R.id.add_car);
+
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUserID = mAuth.getCurrentUser().getUid();
+        UsersRef = FirebaseDatabase.getInstance().getReference().child("User");
+
+
+        mToolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Moje auta");
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawable_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(MainActivity.this, drawerLayout, R.string.drawer_open, R.string.drawer_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        View navView = navigationView.inflateHeaderView(R.layout.navigation_header);
+        NavProfileUserName = (TextView) navView.findViewById(R.id.nav_user_full_name);
+
+        UsersRef.child(currentUserID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot)
+            {
+                if(dataSnapshot.exists())
+                {
+                    if(dataSnapshot.hasChild("username"))
+                    {
+                        String fullname = dataSnapshot.child("username").getValue().toString();
+                        NavProfileUserName.setText(fullname);
+                    }
+
+
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item)
+            {
+                UserMenuSelector(item);
+                return false;
+            }
+        });
+
+        AddCar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SendUserToAddCar();
+            }
+        });
+    }
+
+
+
+
+
+    private void SendUserToLoginActivity() {
+
+        Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+        loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(loginIntent);
+        finish();
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(actionBarDrawerToggle.onOptionsItemSelected(item))
+        {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void UserMenuSelector(MenuItem item) {
+
+        switch (item.getItemId())
+        {
+
+            case R.id.nav_logout:
+                mAuth.signOut();
+                SendUserToLoginActivity();
+                break;
+        }
+
+    }
+    private void SendUserToAddCar() {
+
+        Intent AddCarIntent = new Intent(MainActivity.this, AddCar.class);
+        AddCarIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(AddCarIntent);
+        finish();
+    }
+}
---
+++ app/src/main/java/com/example/serwisauta2/RegisterActivity.java
@@ -0,0 +1,172 @@
+package com.example.serwisauta2;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.jar.Attributes;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private EditText UserName, UserEmail, UserPassword, UserConfirmPassword;
+    private Button CreateAccButton;
+    private TextView AlreadyHaveAcc;
+    private ProgressDialog loadingBar;
+
+    private FirebaseAuth mAuth;
+
+    private static final String TAG = "RegisterActivity";
+    private User user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        AlreadyHaveAcc = (TextView) findViewById(R.id.login_acc_link);
+        UserEmail = (EditText) findViewById(R.id.registerEmail);
+        UserPassword = (EditText) findViewById(R.id.registerPassword);
+        UserName = (EditText) findViewById(R.id.registerName);
+        UserConfirmPassword = (EditText) findViewById(R.id.registerConfirmPassword);
+        CreateAccButton = (Button) findViewById(R.id.buttonRegister);
+        loadingBar = new ProgressDialog(this);
+
+
+        mAuth = FirebaseAuth.getInstance();
+
+        AlreadyHaveAcc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SendUserToLoginActivity();
+            }
+        });
+
+
+
+        CreateAccButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = UserEmail.getText().toString();
+                String password = UserPassword.getText().toString();
+                String confirmpassword = UserConfirmPassword.getText().toString();
+
+                if(TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Proszę wprowadzić email...",Toast.LENGTH_SHORT).show();
+
+                }
+                else if(TextUtils.isEmpty(password))
+                {
+                    Toast.makeText(getApplicationContext(),"Proszę wprowadzić hasło...",Toast.LENGTH_SHORT).show();
+                }
+                else if(TextUtils.isEmpty(confirmpassword))
+                {
+                    Toast.makeText(getApplicationContext(), "Proszę potwierdzić hasło...",Toast.LENGTH_SHORT).show();
+                }
+                else if(!password.equals(confirmpassword))
+                {
+                    Toast.makeText(getApplicationContext(),"Hasła nie zgadzają się...",Toast.LENGTH_SHORT).show();
+                }
+                else {
+
+                    String username = UserName.getText().toString();
+                    user = new User(email, password, username);
+                    registerUser(email, password);
+                    loadingBar.setTitle("Rejestracja konta");
+                    loadingBar.setMessage("Proszę czekać, trwa rejestracja twojego konta...");
+                    loadingBar.show();
+                    loadingBar.setCanceledOnTouchOutside(true);
+                }
+            }
+        });
+
+
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if(currentUser != null)
+        {
+            SendUserToMainActivity();
+        }
+    }
+
+    public void registerUser(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful())
+                        {
+                            mAuth.getCurrentUser().sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful()) {
+                                        Toast.makeText(getApplicationContext(), "Pomyślnie zarejestrowano konto. Zweryfikuj swój email.", Toast.LENGTH_SHORT).show();
+                                        userData();
+                                        startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                                        mAuth.signOut();
+
+                                    }else{
+                                        Toast.makeText(getApplicationContext(), "Rejestracja nie przebiegła pomyślnie, spróbuj ponownie", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                            loadingBar.dismiss();
+
+                        } else
+                            {
+
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+
+                    }
+                });
+    }
+    public void userData() {
+        String username = UserName.getText().toString();
+        String email = UserEmail.getText().toString();
+        String password = UserPassword.getText().toString();
+
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = firebaseDatabase.getReference("User").child(mAuth.getUid());
+        user = new User(username, email, password);
+        databaseReference.setValue(user);
+    }
+    private void SendUserToLoginActivity() {
+        Intent loginIntent = new Intent(RegisterActivity.this, LoginActivity.class);
+        startActivity(loginIntent);
+
+    }
+    private void SendUserToMainActivity() {
+        Intent mainIntent = new Intent(RegisterActivity.this, MainActivity.class);
+        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(mainIntent);
+        finish();
+    }
+}
